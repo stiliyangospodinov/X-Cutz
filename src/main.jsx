5e392006d8491ff586cdc3cc59e352edfdbf7906
stiliyan-gospodinov <stelko095@gmail.com>
2024-10-22 15:24:19 +0300

    Back

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import { HashRouter } from 'react-router-dom';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </HashRouter>
-);
+  <BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  </BrowserRouter>
+)
